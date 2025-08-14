a079b72d509eeacc7fa27fdab715410d5ed78a4e
vincenzov01 <aucellov01@gmail.com>
2025-08-14 22:10:19 +0200

    Base files for Beacon SKD

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,55 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
-    vueDevTools(),
+    vueJsx(),
+    VueDevTools(),
   ],
+  base:
+    mode === "production"
+    ? `${process.env.VITE_SHEET_PATH}/${process.env.VITE_SHEET_SHORT_NAME}/`
+    : "/",
+  build: {
+    target: 'esnext',
+    emptyOutDir: true,
+    minify: true,
+    cssCodeSplit: false,
+    rollupOptions: {
+      input: {
+        sheet: "src/main.ts"
+      },
+      output: {
+        dir: "dist",
+        compact: false,
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "style.css") return "sheet.css";
+          return "assets/[name][extname]";
+        },
+        entryFileNames: "sheet.js",
+        minifyInternalExports: false
+      }
+    }
+  },
+  assetsInclude: ["**/*.hbs"],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
-})
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis"
+      }
+    }
+  },
+  server: {
+    cors: false
+  }
+}))
